3704753a8af08066191f3bd7213530bb5940370d
BrightHuangWei <358410916@qq.com>
2018-03-25 19:02:56 +0800

    重新提交

---
+++ business/src/main/java/com/huangwei/business/icon/BusinessIcons.java
@@ -0,0 +1,30 @@
+package com.huangwei.business.icon;
+
+import com.joanzapata.iconify.Icon;
+
+/**
+ * Created by HuangWei on 2018/3/25.
+ */
+
+public enum BusinessIcons implements Icon {
+    icon_scan('\ue610'),
+    icon_ali_pay('\ue659')
+    ;
+
+    private char character;
+
+    BusinessIcons(char character) {
+        this.character = character;
+    }
+
+
+    @Override
+    public String key() {
+        return name().replace('_','-');
+    }
+
+    @Override
+    public char character() {
+        return character;
+    }
+}
---
+++ business/src/main/java/com/huangwei/business/icon/FontBusinessModule.java
@@ -0,0 +1,22 @@
+package com.huangwei.business.icon;
+
+import com.joanzapata.iconify.Icon;
+import com.joanzapata.iconify.IconFontDescriptor;
+
+/**
+ * Created by HuangWei on 2018/3/25.
+ */
+
+public class FontBusinessModule implements IconFontDescriptor {
+
+
+    @Override
+    public String ttfFileName() {
+        return "iconfont.ttf";
+    }
+
+    @Override
+    public Icon[] characters() {
+        return BusinessIcons.values();
+    }
+}
